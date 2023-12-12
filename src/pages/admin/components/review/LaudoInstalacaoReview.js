6e8e462ea952f79e9e7d9708e85ceb68ea2a5572
Thiago Papim <thiago.fourp@gmail.com>
2023-12-12 14:50:08 -0300

    concluido pdf laudo2

--- src/pages/admin/components/review/LaudoInstalacaoReview.js
+++ src/pages/admin/components/review/LaudoInstalacaoReview.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-magic-numbers */
 /* eslint-disable react/jsx-max-depth */
 /* eslint-disable no-unused-vars */
-import { Badge, Button, FormControlLabel, FormGroup, Switch } from '@mui/material';
+import { Badge, Box, Button, CircularProgress,
+  FormControlLabel, FormGroup, Switch } from '@mui/material';
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
@@ -9,18 +10,16 @@ import AppContext from '../../../../context/AppContext';
 import gerarPdfLaudoInstalacao from '../laudoInstalacao/gerarPdfLaudoInstalacao';
 
 export default function LaudoInstalacaoReview() {
-  const {
-    spdaEdit, setSpdaEdit,
-    laudoInstalacaoEdit, setLaudoInstalacaoEdit,
-  } = useContext(AppContext);
-  const [spdas, setSpdas] = useState([]);
-  const [laudoInstalacoes, setLaudoInstalacoes] = useState([]);
+  const { setLaudoInstalacaoEdit } = useContext(AppContext);
   const history = useHistory();
+  const [laudoInstalacoes, setLaudoInstalacoes] = useState([]);
   const [complete, setComplete] = useState(0);
   const [incluirFinalizados, setIncluirFinalizados] = useState(false);
+  const [loading, setLoading] = useState(false);
   const localhost = process.env.REACT_APP_LOCAL_HOST;
 
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
       try {
         const url = incluirFinalizados
@@ -30,7 +29,9 @@ export default function LaudoInstalacaoReview() {
           return new Date(b.createdAt) - new Date(a.createdAt);
         });
         setLaudoInstalacoes(sortedLaudos);
+        setLoading(false);
       } catch (error) {
+        setLoading(false);
         console.error(error);
       }
     };
@@ -84,113 +85,121 @@ export default function LaudoInstalacaoReview() {
   };
 
   return (
-    <div className="App">
-      <FormGroup>
-        <FormControlLabel
-          control={ <Switch /> }
-          label="Incluir Finalizados"
-          onChange={ () => setIncluirFinalizados(!incluirFinalizados) }
-        />
-      </FormGroup>
-      <div>
-        <table className="border-2 border-gray-300 w-full">
-          <thead>
-            <tr>
-              <th>Status</th>
-              <th className="border-x-2 border-gray-300">Nome</th>
-              <th className="border-x-2 border-gray-300">Empresa</th>
-              <th className="border-x-2 border-gray-300">Data</th>
-              <th>Editar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {laudoInstalacoes.map((laudoInstalacoess, i) => (
-              <tr
-                key={ i }
-                className={
-                  `border-y-2 ${i % 2 === 0 ? 'bg-gray-200' : 'bg-gray-100'}`
-                }
-              >
-                <td>
-                  {laudoInstalacoess.complete ? (
-                    <div className="flex justify-center items-center">
-                      <Badge
-                        color="success"
-                        variant="dot"
-                        className="mr-2"
-                      />
-                      <p>
-                        Finalizado
-                      </p>
-                    </div>
-                  ) : (
-                    <div className="flex justify-center items-center">
-                      <Badge
-                        color="error"
-                        variant="dot"
-                        className="mr-2"
-                      />
-                      <p>
-                        Em Aberto
-                      </p>
-                    </div>
-                  )}
-                </td>
-                <td
-                  className="border-x-2 border-gray-300"
-                >
-                  {laudoInstalacoess.nome}
-                </td>
-                <td
-                  className="border-x-2 border-gray-300"
-                >
-                  {laudoInstalacoess.empresa.nome}
-                </td>
-                <td
-                  className="border-x-2 border-gray-300"
-                >
-                  {formatDate(laudoInstalacoess.createdAt)}
-                </td>
-                <td>
-                  <div>
-                    <Button
-                      className="m-1"
-                      variant="contained"
-                      onClick={ () => handleEdit(laudoInstalacoess.id) }
+    <div className="App flex flex-col">
+      { loading ? (
+        <Box className="flex w-full justify-center">
+          <CircularProgress />
+        </Box>
+      ) : (
+        <div>
+          <FormGroup>
+            <FormControlLabel
+              control={ <Switch checked={ incluirFinalizados } /> }
+              label="Incluir Finalizados"
+              onChange={ () => setIncluirFinalizados(!incluirFinalizados) }
+            />
+          </FormGroup>
+          <div>
+            <table className="border-2 border-gray-300 w-full">
+              <thead>
+                <tr>
+                  <th>Status</th>
+                  <th className="border-x-2 border-gray-300">Nome</th>
+                  <th className="border-x-2 border-gray-300">Empresa</th>
+                  <th className="border-x-2 border-gray-300">Data</th>
+                  <th>Editar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {laudoInstalacoes.map((laudoInstalacoess, i) => (
+                  <tr
+                    key={ i }
+                    className={
+                      `border-y-2 ${i % 2 === 0 ? 'bg-gray-200' : 'bg-gray-100'}`
+                    }
+                  >
+                    <td>
+                      {laudoInstalacoess.complete ? (
+                        <div className="flex justify-center items-center">
+                          <Badge
+                            color="success"
+                            variant="dot"
+                            className="mr-2"
+                          />
+                          <p>
+                            Finalizado
+                          </p>
+                        </div>
+                      ) : (
+                        <div className="flex justify-center items-center">
+                          <Badge
+                            color="error"
+                            variant="dot"
+                            className="mr-2"
+                          />
+                          <p>
+                            Em Aberto
+                          </p>
+                        </div>
+                      )}
+                    </td>
+                    <td
+                      className="border-x-2 border-gray-300"
                     >
-                      Editar
+                      {laudoInstalacoess.nome}
+                    </td>
+                    <td
+                      className="border-x-2 border-gray-300"
+                    >
+                      {laudoInstalacoess.empresa.nome}
+                    </td>
+                    <td
+                      className="border-x-2 border-gray-300"
+                    >
+                      {formatDate(laudoInstalacoess.createdAt)}
+                    </td>
+                    <td>
+                      <div>
+                        <Button
+                          className="m-1"
+                          variant="contained"
+                          onClick={ () => handleEdit(laudoInstalacoess.id) }
+                        >
+                          Editar
 
-                    </Button>
-                    <Button
-                      className="m-1"
-                      variant="contained"
-                      onClick={ () => deleteSpda(laudoInstalacoess.id) }
-                    >
-                      Deletar
+                        </Button>
+                        <Button
+                          className="m-1"
+                          variant="contained"
+                          onClick={ () => deleteSpda(laudoInstalacoess.id) }
+                        >
+                          Deletar
 
-                    </Button>
-                    <Button
-                      className="m-1"
-                      variant="contained"
-                      onClick={ () => finalizar(laudoInstalacoess.id) }
-                    >
-                      Finalizar
+                        </Button>
+                        <Button
+                          className="m-1"
+                          variant="contained"
+                          onClick={ () => finalizar(laudoInstalacoess.id) }
+                        >
+                          Finalizar
 
-                    </Button>
-                    <Button
-                      className="m-1"
-                      variant="contained"
-                      onClick={ () => baixarPdf(laudoInstalacoess) }
-                    >
-                      Baixar PDF
-                    </Button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+                        </Button>
+                        <Button
+                          className="m-1"
+                          variant="contained"
+                          onClick={ () => baixarPdf(laudoInstalacoess) }
+                        >
+                          Baixar PDF
+                        </Button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ) }
     </div>
   );
 }
--- src/pages/admin/components/review/SpdaReview.js
+++ src/pages/admin/components/review/SpdaReview.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-magic-numbers */
 /* eslint-disable react/jsx-max-depth */
 /* eslint-disable no-unused-vars */
-import { Badge, Button, FormControlLabel, FormGroup, Switch } from '@mui/material';
+import { Badge,
+  Box, Button, CircularProgress,
+  FormControlLabel, FormGroup, Switch } from '@mui/material';
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
@@ -9,14 +11,16 @@ import AppContext from '../../../../context/AppContext';
 import gerarPdfSpda from '../spda/gerarPdfSpda';
 
 export default function SpdaReview() {
-  const { spdaEdit, setSpdaEdit } = useContext(AppContext);
+  const { setSpdaEdit } = useContext(AppContext);
   const [spdas, setSpdas] = useState([]);
-  const history = useHistory();
   const [complete, setComplete] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
   const [incluirFinalizados, setIncluirFinalizados] = useState(false);
   const localhost = process.env.REACT_APP_LOCAL_HOST;
 
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
       try {
         const url = incluirFinalizados
@@ -26,7 +30,9 @@ export default function SpdaReview() {
           return new Date(b.createdAt) - new Date(a.createdAt);
         });
         setSpdas(sortedSpdas);
+        setLoading(false);
       } catch (error) {
+        setLoading(false);
         console.error(error);
       }
     };
@@ -73,105 +79,113 @@ export default function SpdaReview() {
   };
 
   return (
-    <div className="App">
-      <FormGroup>
-        <FormControlLabel
-          control={ <Switch /> }
-          label="Incluir Finalizados"
-          onChange={ () => setIncluirFinalizados(!incluirFinalizados) }
-        />
-      </FormGroup>
-      <div>
-        <table className="border-2 border-gray-300 w-full">
-          <thead>
-            <tr>
-              <th>Status</th>
-              <th className="border-x-2 border-gray-300">Nome</th>
-              <th className="border-x-2 border-gray-300">Empresa</th>
-              <th className="border-x-2 border-gray-300">Data</th>
-              <th>Editar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {spdas.map((spdaa, i) => (
-              <tr
-                key={ i }
-                className={
-                  `border-y-2 ${i % 2 === 0 ? 'bg-gray-200' : 'bg-gray-100'}`
-                }
-              >
-                <td>
-                  {spdaa.complete ? (
-                    <div className="flex justify-center items-center">
-                      <Badge
-                        color="success"
-                        variant="dot"
-                        className="mr-2"
-                      />
-                      <p>
-                        Finalizado
-                      </p>
-                    </div>
-                  ) : (
-                    <div className="flex justify-center items-center">
-                      <Badge
-                        color="error"
-                        variant="dot"
-                        className="mr-2"
-                      />
-                      <p>
-                        Em Aberto
-                      </p>
-                    </div>
-                  )}
-                </td>
-                <td className="border-x-2 border-gray-300">{spdaa.nome}</td>
-                <td className="border-x-2 border-gray-300">{spdaa.empresa?.nome}</td>
-                <td
-                  className="border-x-2 border-gray-300"
-                >
-                  {formatDate(spdaa.createdAt)}
-                </td>
-                <td>
-                  <div>
-                    <Button
-                      className="m-1"
-                      variant="contained"
-                      onClick={ () => handleEdit(spdaa.id) }
+    <div className="App flex flex-col">
+      { loading ? (
+        <Box className="flex w-full justify-center">
+          <CircularProgress />
+        </Box>
+      ) : (
+        <div>
+          <FormGroup>
+            <FormControlLabel
+              control={ <Switch checked={ incluirFinalizados } /> }
+              label="Incluir Finalizados"
+              onChange={ () => setIncluirFinalizados(!incluirFinalizados) }
+            />
+          </FormGroup>
+          <div>
+            <table className="border-2 border-gray-300 w-full">
+              <thead>
+                <tr>
+                  <th>Status</th>
+                  <th className="border-x-2 border-gray-300">Nome</th>
+                  <th className="border-x-2 border-gray-300">Empresa</th>
+                  <th className="border-x-2 border-gray-300">Data</th>
+                  <th>Editar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {spdas.map((spdaa, i) => (
+                  <tr
+                    key={ i }
+                    className={
+                      `border-y-2 ${i % 2 === 0 ? 'bg-gray-200' : 'bg-gray-100'}`
+                    }
+                  >
+                    <td>
+                      {spdaa.complete ? (
+                        <div className="flex justify-center items-center">
+                          <Badge
+                            color="success"
+                            variant="dot"
+                            className="mr-2"
+                          />
+                          <p>
+                            Finalizado
+                          </p>
+                        </div>
+                      ) : (
+                        <div className="flex justify-center items-center">
+                          <Badge
+                            color="error"
+                            variant="dot"
+                            className="mr-2"
+                          />
+                          <p>
+                            Em Aberto
+                          </p>
+                        </div>
+                      )}
+                    </td>
+                    <td className="border-x-2 border-gray-300">{spdaa.nome}</td>
+                    <td className="border-x-2 border-gray-300">{spdaa.empresa?.nome}</td>
+                    <td
+                      className="border-x-2 border-gray-300"
                     >
-                      Editar
+                      {formatDate(spdaa.createdAt)}
+                    </td>
+                    <td>
+                      <div>
+                        <Button
+                          className="m-1"
+                          variant="contained"
+                          onClick={ () => handleEdit(spdaa.id) }
+                        >
+                          Editar
 
-                    </Button>
-                    <Button
-                      className="m-1"
-                      variant="contained"
-                      onClick={ () => deleteSpda(spdaa.id) }
-                    >
-                      Deletar
+                        </Button>
+                        <Button
+                          className="m-1"
+                          variant="contained"
+                          onClick={ () => deleteSpda(spdaa.id) }
+                        >
+                          Deletar
 
-                    </Button>
-                    <Button
-                      className="m-1"
-                      variant="contained"
-                      onClick={ () => finalizar(spdaa.id) }
-                    >
-                      Finalizar
+                        </Button>
+                        <Button
+                          className="m-1"
+                          variant="contained"
+                          onClick={ () => finalizar(spdaa.id) }
+                        >
+                          Finalizar
 
-                    </Button>
-                    <Button
-                      className="m-1"
-                      variant="contained"
-                      onClick={ () => baixarPdf(spdaa) }
-                    >
-                      Baixar PDF
-                    </Button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+                        </Button>
+                        <Button
+                          className="m-1"
+                          variant="contained"
+                          onClick={ () => baixarPdf(spdaa) }
+                        >
+                          Baixar PDF
+                        </Button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
